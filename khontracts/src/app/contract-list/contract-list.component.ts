894cad78a1a551f920afd1c0ff87603185d6d39b
mmqadi <mmqadi@users.noreply.github.com>
2021-03-13 17:38:27 +0200

    added contact list files

---
+++ khontracts/src/app/contract-list/contract-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contract-list',
+  templateUrl: './contract-list.component.html',
+  styleUrls: ['./contract-list.component.css'],
+})
+export class ContractListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
